14b008cd0f92c10cc0665464208440ddf428e3b7
Drake Olson <drake.olson.96@gmail.com>
2024-10-11 15:43:23 -0500

    Intune - Remediations - Windows Activation
    Added Windows activation detect and remediate scripts.

---
+++ Remidiate-WindowsActivation.ps1
@@ -0,0 +1,24 @@
+Start-Transcript -Path C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Remediations\Remidiate-WindowsActivation.log -Append
+
+$MAKKey = "" #Fill in the value here if you want to use a MAK key to activate. If empty, motherboard key will be used.
+
+$activationKey = ""
+
+if($MAKKey -eq ""){
+    Write-Output "Activating Windows with Motherboard Key"
+    $activationKey = (Get-CimInstance -Query 'select * from SoftwareLicensingService').OA3xOriginalProductKey
+}
+else{
+    Write-Output "Activating Windows with MAK Key"
+    $activationKey = $MAKKey
+}
+
+try{
+    Write-Output "Key used: $activationKey"
+    slmgr.vbs /ato
+    Write-Output "Windows Activation attempt complete!"
+}
+catch{
+    Write-Output "Windows could not successfully activate: $($_.Exception.Message)"
+}
+Stop-Transcript
---
+++ Windows Activation/Detect-WindowsActivation.ps1
@@ -0,0 +1,17 @@
+Start-Transcript -Path C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Remediations\Detect-WindowsActivation.log -Append
+
+# Get Windows License Status Code
+$licenseStatusCode = (Get-CimInstance -ClassName SoftwareLicensingPRoduct | Where-Object {$_.PartialProductKey}).LicenseStatus
+
+# Windows is only activated if the code is 0
+if($licenseStatusCode -ne 1){
+    Write-Output "Windows is not activated."
+    Stop-Transcript
+    exit -1
+}
+else{
+    Write-Output "Windows is activated."
+    Stop-Transcript
+    exit 0
+}
+
